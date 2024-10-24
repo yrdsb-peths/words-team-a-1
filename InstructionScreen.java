5cb4acb8bb0932e211e2d7c6ed3b1740413df94c
Carlisa100 <348823873@gapps.yrdsb.ca>
2024-10-24 12:44:07 -0400

    Create Instruction Class

---
+++ InstructionScreen.java
@@ -0,0 +1,43 @@
+import greenfoot.*;
+
+public class InstructionScreen extends World {
+    private String[] instructions = {
+        "Instruction 1: How to play the game.",
+        "Instruction 2: Use arrow keys to move.",
+        "Instruction 3: Collect items to score."
+    };
+    private int currentIndex = 0;
+    private Label instructionLabel;
+    private Button nextButton;
+
+    public InstructionScreen() {
+        super(600, 400, 1);
+        instructionLabel = new Label(instructions[currentIndex]);
+        addObject(instructionLabel, 300, 200); 
+
+        addObject(new Button(this::previousInstruction), 100, 300); 
+        addObject(new Button(this::nextInstruction), 500, 300); 
+    }
+
+    private void previousInstruction() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateInstruction();
+        }
+    }
+
+    private void nextInstruction() {
+        if (currentIndex < instructions.length - 1) {
+            currentIndex++;
+            updateInstruction();
+        }
+        else
+        {
+             //goToAvatarSelection();
+        }
+    }
+
+    private void updateInstruction() {
+        instructionLabel.setText(instructions[currentIndex]);
+    }
+}
