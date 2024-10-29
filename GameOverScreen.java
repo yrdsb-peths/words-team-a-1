2d765ad5c19eb3b841e6902be1c7eea6052d1286
MaggieJiang7 <147207333+MaggieJiang7@users.noreply.github.com>
2024-10-29 13:01:18 -0400

    Rework scores class

--- GameOverScreen.java
+++ GameOverScreen.java
@@ -1,3 +1,32 @@
-public class GameOverScreen {
-    
+import greenfoot.*;
+
+public class GameOverScreen extends World {
+    Label gameOverLabel = new Label("Game Over", 60);
+    Label highscoresLabel = new Label("High Scores", 40);
+    Label scoreLabel1 = new Label(Scores.returnScoreValue(1), 30);
+    // Label scoreLabel2 = new Label("Game Over", 30);
+    // Label scoreLabel3 = new Label("Game Over", 30);
+
+
+    // Labek score
+ 
+    public GameOverScreen() {
+        super(600, 400, 1); 
+        addObject(gameOverLabel, 250, 100);
+        addObject(highscoresLabel, 250, 150);
+        System.out.println(Scores.returnScoreValue(1));
+        // addObject(titleLabel, 250, 100);
+        // addObject(titleLabel, 250, 100);
+        // addObject(titleLabel, 250, 100);
+        // addObject(titleLabel, 250, 100);
+        // addObject(titleLabel, 250, 100);
+        // addObject(titleLabel, 250, 100);
+    }
+
+    // Add the game screen when done
+    // public void act() {
+    //     if(Greenfoot.isKeyDown("space")) {
+    //         Greenfoot.setWorld(menuScreen);
+    //     }
+    // }
 }
